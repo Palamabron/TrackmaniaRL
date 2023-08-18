cb3b2cfb3b078840647ae36b23fbeaf9332beb36
Palamabron <szulcak05@gmail.com>
2023-08-18 14:55:05 +0200

    Fixed memory (replay buffer) and model (especially qfunctions)

--- tmrl/custom/interfaces/TM2020InterfaceTrackMap.py
+++ tmrl/custom/interfaces/TM2020InterfaceTrackMap.py
@@ -15,9 +15,9 @@ class TM2020InterfaceTrackMap(TM2020InterfaceLidar):
         self.lidar = None
         self.last_pos = [0, 0]
         self.index = 0
-        self.map_left = np.loadtxt(r'HOME_REDACTED\Desktop\Inz\tmrlBase\tmrl\saved_tracks\tmrl-test\track_left.csv',
+        self.map_left = np.loadtxt('saved_tracks/tmrl-test/track_left.csv',
                                    delimiter=',')
-        self.map_right = np.loadtxt(r'HOME_REDACTED\Desktop\Inz\tmrlBase\tmrl\saved_tracks\tmrl-test\track_right.csv',
+        self.map_right = np.loadtxt('saved_tracks/tmrl-test/track_right.csv',
                                     delimiter=',')
         self.all_observed_track_parts = [[], [], [], [], []]
 
